3fb8c2757c83eff03842653dc0c5d9296fd9bb26
Abdelrahman Salem <asalem20@icloud.com>
2024-03-28 00:43:53 +0200

    merging vscode with github

--- DjangoEHR/settings.py
+++ DjangoEHR/settings.py
@@ -75,24 +75,24 @@ WSGI_APPLICATION = 'DjangoEHR.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 # version 1
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-#    }
+#      'default': {
+#          'ENGINE': 'django.db.backends.sqlite3',
+#          'NAME': BASE_DIR / 'db.sqlite3',
+#      }
+# }
 
 # # version2
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'lab3database',
-        'PASSWORD': 'Mypassword3',
-        'HOST': 'ehrserver.postgres.database.azure.com',
+        'NAME': 'salem1190420',
+        'USER': 'asalem20',
+        'PASSWORD': 'Messime1',
+        'HOST': 'salem1190420.postgres.database.azure.com',
         'PORT': '5432',
         'OPTIONS':{'sslmode':'require'},
     }
